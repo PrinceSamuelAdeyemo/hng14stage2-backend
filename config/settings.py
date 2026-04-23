4e87051feb93eccfc6cba53ea0eac5a0ab5e233b
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2026-04-23 03:43:47 +0100

    ...

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR/'.env')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uj1-!02ns@n7p*9!!d@$%9g-g_cr(#(e%8k_1y@wmq*)_&3a3*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('SECRET_KEY')
